ad4ad727982f01239c9ba7f0284130e76231cc65
Eduard Cavasi <eduardcavasi@gmail.com>
2026-03-27 00:36:37 +0200

    model: delete redundant last access time field

--- src/main/java/org/example/model/search/SearchParams.java
+++ src/main/java/org/example/model/search/SearchParams.java
@@ -22,23 +22,20 @@ public class SearchParams {
     private String queryFileName;
     private String queryFileExtension;
     private String queryFilePath;
+
+
     private boolean needsMetadata;
     private boolean greaterSize = true;
     private Long querySize = -1L;
-    private boolean lastModifiedAfter = true;
 
+    private boolean lastModifiedAfter = true;
     @JsonDeserialize(using = FileTimeJsonDeserializer.class)
     private FileTime queryLastModified;
 
     private boolean createdAfter = true;
-
     @JsonDeserialize(using = FileTimeJsonDeserializer.class)
     private FileTime queryCreated;
 
-    private boolean lastAccessedAfter = true;
-
-    @JsonDeserialize(using = FileTimeJsonDeserializer.class)
-    private FileTime queryLastAccessed;
 
     private boolean needsContent;
     private String queryContent;
